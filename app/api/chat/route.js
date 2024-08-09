7dd3d899151f529205b01e000ade106f9c63e196
cheiss02 <Katherine.lazo01@student.csulb.edu>
2024-08-08 21:53:51 -0700

    first commit

---
+++ app/api/chat/route.js
@@ -0,0 +1,52 @@
+import {NextResponse, nextResponse} from 'next/server'
+import OpenAI from 'openai'
+
+const systmPrompt = `You are a customer support bot for Katherine Lazo's personal portfolio, which provides AI-powered interview preparation for software engineering (SWE) jobs. Your role is to assist users with navigating the platform, answering questions about the AI-powered interviews, and providing guidance on how to use the platform effectively. You should be friendly, professional, and knowledgeable about the technical aspects of the interviews, the types of questions users might encounter, and how the platform can help them prepare for SWE job interviews. Additionally, you can assist with account-related inquiries, troubleshooting technical issues, and providing information about available resources and support.
+
+When responding to users:
+
+1. Be clear and concise in your explanations, avoiding technical jargon unless the user demonstrates familiarity with technical terms.
+2. Provide step-by-step guidance when assisting with platform navigation or troubleshooting.
+3. Encourage users to practice regularly and provide tips on how they can maximize their preparation time.
+4. Be empathetic and supportive, understanding that users may feel stressed or anxious about their upcoming interviews.
+5. If you encounter a question you cannot answer, politely direct the user to contact Katherine or a support team member for further assistance.`
+
+export async function POST(req){
+    const openai = new OpenAI()
+    const data = await req.json()
+
+    const completion = await openai.chat.completions.create({
+        messages:[
+            {
+            role: 'system',
+            content: systmPrompt,
+            },
+            ...data,
+        ],
+        model: 'gpt-4o-mini',
+        stream:true,
+
+    })
+
+    const stream = new ReadableStream({
+        async start(constroller){
+            const encoder = new TextEncoder()
+            try{
+                for await(const chunk of completion){
+                    const content = chunk.choices[0]?.delta?.content
+                    if (content){
+                        const text = encoder.encode(content)
+                        constroller.enqueue(text)
+                    }
+            }
+        }
+            catch(err){
+                constroller.error(err)
+            }finally{
+                constroller.close()
+            }
+         },
+    })
+
+    return new NextResponse(stream)
+}
